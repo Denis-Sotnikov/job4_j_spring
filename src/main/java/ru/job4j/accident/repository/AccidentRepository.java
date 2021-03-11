01ed51157929b77c125977b91979130e7d9f1af8
Denis Sotnikov <sport_piter@mail.ru>
2021-03-11 18:25:30 +0300

    0. Spring Security [#469158]

--- src/main/java/ru/job4j/accident/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accident/repository/AccidentRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accident.model.Accident;
 
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
-
 }
